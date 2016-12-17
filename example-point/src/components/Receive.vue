ff3b98e843a04e93ef4fc2c673701ec40eb4d786
gucci-swallow <swallow965dev@gmail.com>
2016-12-18 01:10:26 +0900

    Didsplay qr

--- example-point/src/components/Receive.vue
+++ example-point/src/components/Receive.vue
@@ -1,15 +1,53 @@
 <template>
   <div>
     receive
+    <div v-html='image'></div>
+    <input type='number' min='0' v-bind:change='createQr()' v-model.number='ammount'>
   </div>
 </template>
 
 <script>
+import Qr from 'qr-image'
+
 export default {
-  name: 'receive'
+  name: 'receive',
+  data () {
+    return {
+      image: '',
+      ammount: 0
+    }
+  },
+  created () {
+  },
+  methods: {
+    createQr () {
+      if (this.ammount < 0 || !this.ammount) return
+      console.log(this.ammount)
+      const qrData = {
+        account: this.$localStorage.get('publicKey'),
+        ammount: this.ammount
+      }
+      const qrString = JSON.stringify(qrData)
+      console.log(qrString)
+      const img = Qr.imageSync(qrString, {type: 'svg'})
+      this.image = img
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+svg{
+  width: 200px;
+}
+
+input[type='number']::-webkit-outer-spin-button,
+input[type='number']::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+input[type='number'] {
+    -moz-appearance:textfield;
+}
 </style>
