bf395fc8050ddac253e25c374f27c0d2a088d18b
George <theofilis.g@gmail.com>
2018-01-09 16:08:47 +0200

    ci: Add configuration for two different libary builds

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,18 @@
 import { join } from "path";
+const _ = require("lodash");
+const minimist = require("minimist");
 const { camelCase } = require("lodash");
 const { TsConfigPathsPlugin, CheckerPlugin } = require("awesome-typescript-loader");
 const TypedocWebpackPlugin = require("typedoc-webpack-plugin");
 
-/**
- * Update this variable if you change your library name
- */
+const DEFAULT_TARGET = "node";
+
 const libraryName = "irohajs";
 
-export default {
+const DEFAULT_PARAMS = {
   entry: join(__dirname, `src/${libraryName}.ts`),
-  // Currently cheap-module-source-map is broken https://github.com/webpack/webpack/issues/4176
+  target: "node",
   devtool: "source-map",
-  output: {
-    path: join(__dirname, "dist"),
-    libraryTarget: "umd",
-    library: camelCase(libraryName),
-    filename: `${libraryName}.js`
-  },
   resolve: {
     extensions: [".ts", ".js"]
   },
@@ -45,10 +40,55 @@ export default {
       },
       "src"
     )
-  ],
+  ]
+};
+const nodeModules = ["grpc", "moment", "axios", "js-sha3", "supercop.js"];
+const PARAMS_PER_TARGET: any = {
   node: {
-    fs: "empty",
-    net: "empty",
-    tls: "empty"
+    target: "node",
+    node: {
+      process: false
+    },
+    externals: nodeModules,
+    output: {
+      path: join(__dirname, "dist"),
+      libraryTarget: "umd",
+      library: camelCase(libraryName),
+      filename: `${libraryName}.js`
+    }
+  },
+  web: {
+    target: "web",
+    node: {
+      fs: "empty",
+      net: "empty",
+      tls: "empty"
+    },
+    output: {
+      path: join(__dirname, "dist"),
+      libraryTarget: "umd",
+      library: camelCase(libraryName),
+      filename: `${libraryName}.browser.js`
+    }
   }
 };
+
+const target: string = _resolveBuildTarget(DEFAULT_TARGET);
+const params: any = _.merge(DEFAULT_PARAMS, PARAMS_PER_TARGET[target], _mergeArraysCustomizer);
+
+export default params;
+
+function _resolveBuildTarget (defaultTarget: string): string {
+  let target = minimist(process.argv.slice(2)).target;
+  if (!target) {
+    console.log("No build target provided, using default target instead\n\n");
+    target = defaultTarget;
+  }
+  return target;
+}
+
+function _mergeArraysCustomizer (a: any, b: any): any {
+  if (_.isArray(a)) {
+    return a.concat(b);
+  }
+}
